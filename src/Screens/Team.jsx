ad012ac968dd581a35f7f6c26ecd8435d4890e3b
Levi Raoelina <levi_r34@yahoo.com>
2023-07-10 15:46:42 -0600

    team work

--- src/Screens/Team.jsx
+++ src/Screens/Team.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import { useState,useEffect,useContext } from 'react'
-import GlobalContext from '../state/GlobalState';
-
+import React from "react";
+import { useState, useEffect, useContext } from "react";
+import GlobalContext from "../state/GlobalState";
+import axios from "axios";
 
 const Team = () => {
-  const {team} = useContext(GlobalContext)
+  const { team } = useContext(GlobalContext);
+
+  useEffect(() => {
+    const newTeam = team.map((name) => {
+      axios
+        .get(` https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+  }, []);
+
   return (
     <div>
-      {team.map((pokemon)=> {
-        return <h1>{pokemon}</h1>
+      {team.map((pokemon) => {
+        return <h1>{pokemon}</h1>;
       })}
-      
     </div>
-  )
-}
+  );
+};
 
-export default Team
+export default Team;
