6529e09616659878cdb52d21e844010cd4666fc0
Levi Raoelina <levi_r34@yahoo.com>
2023-09-11 09:58:07 -0600

    css

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
-import { useContext } from 'react'
-import GlobalContext from '../state/GlobalState'
-
+import React from "react";
+import { useContext } from "react";
+import GlobalContext from "../state/GlobalState";
+import Groundon from '../images/Groudon.png'
+import Kyogre from '../images/Kyogre.png'
 
 const Home = () => {
-  const {state} = useContext(GlobalContext)
+  const { state } = useContext(GlobalContext);
   return (
     <div>
-      <h1>Welcome to PokemonClash, {state.username}!</h1>
+      <div className="welcomeBoxBox">
+        <div className="welcomeBox">
+          <h1>Welcome to PokemonClash, {state.username}!</h1>
+          <h2>A game of luck where power levels are randomized...</h2>
+          <div className="pokemonImages">
+            <img src={Kyogre} alt="" className="kyogre" />
+          <div className="instructions">
+            
+            <div className="stepOne">
+              <h3>1. Add Pokemon to your team</h3>
+            </div>
+            <div className="stepTwo">
+              <h3>2. View your team in the team tab</h3>
+            </div>
+            <div className="stepThree">
+              <h3>3. Battle to see who has the highest team power!</h3>
+            </div>
+          </div>
+          <img src={Groundon} alt="" className="groudon" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
