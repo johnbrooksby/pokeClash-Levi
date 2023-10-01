04e36d448712f0ccb60c1ef232ba3326dd9cfd50
Levi Raoelina <levi_r34@yahoo.com>
2023-09-30 20:43:39 -0600

    links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
-import Swords from '../images/swords.svg'
-import Home from '../images/home.svg'
-import Team from '../images/team.svg'
-import Pokeball from '../images/Pokeball.png'
+import React from "react";
+import Swords from "../images/swords.svg";
+import Home from "../images/home.svg";
+import Team from "../images/team.svg";
+import Pokeball from "../images/Pokeball.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer >
-        <div className='mobile-navbox'>
-        <div className='mobile-nav'>
-            <img src={Home} alt="" className='navIMG' />
-            <img src={Pokeball} alt=""className='navIMG'/>
-            <img src={Team} alt="" className='navIMG'/>
-            <img src={Swords} alt="" className='navIMG'/>
-
-
+    <footer>
+      <div className="mobile-navbox">
+        <div className="mobile-nav">
+          <Link to={'/'}>
+            <img src={Home} alt="" className="navIMG" />
+          </Link>
+          <Link to={'/pokemon'}>
+            <img src={Pokeball} alt="" className="navIMG" />
+          </Link>
+          <Link to={'/team'}>
+            <img src={Team} alt="" className="navIMG" />
+          </Link>
+          <Link to={'/rumble'}>
+            <img src={Swords} alt="" className="navIMG" />
+          </Link>
         </div>
-        </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
